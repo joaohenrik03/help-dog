ad0f27a77899ad1591fdb2f722daf6a72a43602c
João Henrik <jh.signori@gmail.com>
2022-07-08 01:31:01 -0300

    Updated styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,39 @@ import { Logo } from './components/Logo';
 
 export function App() {
   return (
-    <>
-      <div>
-        <span>
+    <div className='grid'>
+      <div className='pt-6 pr-6 pl-6 text-center'>
+        <span className='flex items-center justify-center gap-3 text-purple-600 font-medium mb-16 font-sans'>
           <Logo />
           HelpDog
         </span>
 
         <main>
-          <h1>
-            Ajude quem precisa de ajuda
-          </h1>
-          <span>
-            <svg width="239" height="24" viewBox="0 0 239 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M4 19.6159C4 19.6159 89 -7.72852 235.5 9.95114" stroke="#713FFF" stroke-width="7" stroke-linecap="round" />
+          <div className='mb-8'>
+            <h1 className='text-purple-600 text-3xl font-extrabold'>
+              Ajude quem precisa de ajuda
+            </h1>
+            <svg className='w-[50%]' height="24" viewBox="0 0 239 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M4 19.6159C4 19.6159 89 -7.72852 235.5 9.95114" stroke="#713FFF" stroke-width="7" stroke-linecap="round" />
             </svg>
-          </span>
+          </div>
 
-          <p>
+          <p className='purple-600 font-normal mb-8'>
             Com apenas 1 real você pode me ajudar a alimentar cães de ruas.
           </p>
 
-          <a href="#">
-            Contribuir
-          </a>
-          <a href="#">
-            Entrar em contato
-          </a>
+          <div className='grid gap-6 justify-center'>
+            <a href="#" className='text-lg bg-purple-500 px-12 py-3 rounded-[40px] text-white max-w-[180px]'>
+              Contribuir
+            </a>
+            <a href="#" className='text-lg text-purple-600'>
+              Entrar em contato
+            </a>
+          </div>
         </main>
       </div>
       <div>
-        
       </div>
-    </>
+    </div>
   )
 }
