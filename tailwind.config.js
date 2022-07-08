132a646d93468cd332982a22159d18b558326d02
João Henrik <jh.signori@gmail.com>
2022-07-08 01:05:16 -0300

    Colors and font configured in tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      extend: {
+        fontFamily: {
+          sans: 'Rubik, sans-serif;',       
+          bold: 'Merriweather, serif;',
+        },
+        colors: {
+          purple: {
+            500: '#713FFF',
+            600: '#3C184E',
+          }
+        } 
+      }
+    },
   },
   plugins: [],
 }
